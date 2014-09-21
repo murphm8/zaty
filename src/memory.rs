abe1a440f24b92252a22200baa98dcd2f73702b0
Matt Murphy <matthew.john.murphy@gmail.com>
2014-09-21 16:16:14 -0500

    Convert memory::Memory to use Cell

--- src/memory.rs
+++ src/memory.rs
@@ -2,33 +2,34 @@ use std::rand::Rng;
 use std::rand;
 use std::iter::range_inclusive;
 use std::iter::range_step_inclusive;
+use std::cell::Cell;
 
 pub struct Memory {
-    mem: [u8, ..65536] 
+    mem: [Cell<u8>, ..65536] 
 }
 
 impl Memory {
     pub fn new() -> Memory {
-        return Memory{ mem: [0, ..65536]} 
+        return Memory{ mem: [Cell::new(0), ..65536]} 
     }
 
     pub fn read_byte(&self, addr: u16) -> u8 {
-        return self.mem[addr as uint];
+        return self.mem[addr as uint].get();
     }
 
     pub fn read_word(&self, addr: u16) -> u16 {
-        let high_byte = self.mem[(addr + 1) as uint];
-        let low_byte = self.mem[addr as uint];
+        let high_byte = self.mem[(addr + 1) as uint].get();
+        let low_byte = self.mem[addr as uint].get();
         return ((high_byte as u16) << 8) + (low_byte as u16);
     }
 
-    pub fn write_byte(&mut self, addr: u16, data: u8) {
-        self.mem[addr as uint] = data;
+    pub fn write_byte(&self, addr: u16, data: u8) {
+        self.mem[addr as uint].set(data);
     }
 
-    pub fn write_word(&mut self, addr: u16, data: u16) {
-        self.mem[addr as uint] = low_byte(data);
-        self.mem[(addr + 1) as uint] = high_byte(data);
+    pub fn write_word(&self, addr: u16, data: u16) {
+        self.mem[addr as uint].set(low_byte(data));
+        self.mem[(addr + 1) as uint].set(high_byte(data));
     }
 }
 
@@ -95,7 +96,7 @@ fn write_byte_should_write_to_mem() {
     {
         let num = rng.gen::<u8>();
         memory.write_byte(n, num);
-        assert!(memory.mem[n as uint] == num);
+        assert!(memory.mem[n as uint].get() == num);
     }
 }
 
@@ -108,8 +109,8 @@ fn write_word_should_write_word_to_mem_little_endian() {
     {
         let num = rng.gen::<u16>();
         memory.write_word(n, num);
-        assert!(memory.mem[n as uint] == low_byte(num));
-        assert!(memory.mem[(n + 1) as uint] == high_byte(num));
+        assert!(memory.mem[n as uint].get() == low_byte(num));
+        assert!(memory.mem[(n + 1) as uint].get() == high_byte(num));
     }
 }
 
@@ -117,13 +118,13 @@ fn write_word_should_write_word_to_mem_little_endian() {
 fn read_byte_should_read_byte() {
     let mut memory = Memory::new();
     
-    memory.mem[0] = 0xF1;
+    memory.mem[0].set(0xF1);
     assert!(memory.read_byte(0) == 0xF1);
 
-    memory.mem[0xFFFF] = 0xAB;
+    memory.mem[0xFFFF].set(0xAB);
     assert!(memory.read_byte(0xFFFF) == 0xAB);
 
-    memory.mem[0xABAB] = 0xBB;
+    memory.mem[0xABAB].set(0xBB);
     assert!(memory.read_byte(0xABAB) == 0xBB);
 }
 
@@ -131,15 +132,15 @@ fn read_byte_should_read_byte() {
 fn read_word_should_read_word() {
     let mut memory = Memory::new();
     
-    memory.mem[0] = 0xF1;
-    memory.mem[1] = 0xEA;
+    memory.mem[0].set(0xF1);
+    memory.mem[1].set(0xEA);
     assert!(memory.read_word(0) == 0xEAF1);
 
-    memory.mem[0xFFFE] = 0xBB;
-    memory.mem[0xFFFF] = 0xAB;
+    memory.mem[0xFFFE].set(0xBB);
+    memory.mem[0xFFFF].set(0xAB);
     assert!(memory.read_word(0xFFFE) == 0xABBB);
 
-    memory.mem[0xABAA] = 0x12;
-    memory.mem[0xABAB] = 0xBA;
+    memory.mem[0xABAA].set(0x12);
+    memory.mem[0xABAB].set(0xBA);
     assert!(memory.read_word(0xABAA) == 0xBA12);
 }
