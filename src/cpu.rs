19d96906a1589b0aeb6785efda1a71469fa80699
Matt Murphy <matthew.john.murphy@gmail.com>
2014-10-14 22:53:20 -0500

    Add 0x10-0x15 opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -22,22 +22,28 @@ impl<'a> Cpu<'a> {
         let instr = self.fetch_instruction(); 
 
         match instr {
-            0x00 => ops::nop(),
-            0x01 => ops::ld_next_two_byte_into_reg_pair(self.mem, &mut self.reg.pc, &mut self.reg.b, &mut self.reg.c),
-            0x02 => ops::write_value_to_memory_at_address(self.mem, self.reg.a.read(), self.reg.b.read(), self.reg.c.read()),
-            0x03 => ops::increment_register_pair(&mut self.reg.b, &mut self.reg.c),
-            0x04 => ops::increment_register(&mut self.reg.b, &mut self.reg.f),
-            0x05 => ops::decrement_register(&mut self.reg.b, &mut self.reg.f),
-            0x06 => ops::ld_immediate(self.mem, &mut self.reg.pc, &mut self.reg.b),
-            0x07 => ops::rotate_left_with_carry(&mut self.reg.a, &mut self.reg.f),
-            0x08 => ops::write_sp_to_address_immediate(self.mem, &mut self.reg.pc, &self.reg.sp),
-            0x09 => ops::add_register_pair_to_register_pair(&mut self.reg.h, &mut self.reg.l, &self.reg.b, &self.reg.c, &mut self.reg.f),
-            0x0A => ops::ld_a_from_reg_pair_as_address(self.mem, &mut self.reg.a, &mut self.reg.b, &mut self.reg.c),
-            0x0B => ops::decrement_register_pair(&mut self.reg.b, &mut self.reg.c),
-            0x0C => ops::increment_register(&mut self.reg.c, &mut self.reg.f),
-            0x0D => ops::decrement_register(&mut self.reg.c, &mut self.reg.f),
-            0x0E => ops::ld_immediate(self.mem, &mut self.reg.pc, &mut self.reg.c),
-            0x0F => ops::rotate_right_with_carry(&mut self.reg.a, &mut self.reg.f),
+            0x00 => ops::nop(), // NOP
+            0x01 => ops::ld_next_two_byte_into_reg_pair(self.mem, &mut self.reg.pc, &mut self.reg.b, &mut self.reg.c), // LD BC, nn
+            0x02 => ops::write_value_to_memory_at_address(self.mem, self.reg.a.read(), self.reg.b.read(), self.reg.c.read()), // LD (BC), A
+            0x03 => ops::increment_register_pair(&mut self.reg.b, &mut self.reg.c), // INC BC
+            0x04 => ops::increment_register(&mut self.reg.b, &mut self.reg.f), // INC B
+            0x05 => ops::decrement_register(&mut self.reg.b, &mut self.reg.f), // DEC B
+            0x06 => ops::ld_immediate(self.mem, &mut self.reg.pc, &mut self.reg.b), // LD B, n
+            0x07 => ops::rotate_left_with_carry(&mut self.reg.a, &mut self.reg.f), // RLC A
+            0x08 => ops::write_sp_to_address_immediate(self.mem, &mut self.reg.pc, &self.reg.sp), // LD (nn), SP
+            0x09 => ops::add_register_pair_to_register_pair(&mut self.reg.h, &mut self.reg.l, &self.reg.b, &self.reg.c, &mut self.reg.f), // ADD HL, BC
+            0x0A => ops::ld_a_from_reg_pair_as_address(self.mem, &mut self.reg.a, &mut self.reg.b, &mut self.reg.c), // LD A, (BC)
+            0x0B => ops::decrement_register_pair(&mut self.reg.b, &mut self.reg.c), // DEC BC
+            0x0C => ops::increment_register(&mut self.reg.c, &mut self.reg.f), // INC C
+            0x0D => ops::decrement_register(&mut self.reg.c, &mut self.reg.f), // DEC C
+            0x0E => ops::ld_immediate(self.mem, &mut self.reg.pc, &mut self.reg.c), // LD C, n
+            0x0F => ops::rotate_right_with_carry(&mut self.reg.a, &mut self.reg.f), // RRC A
+            0x10 => return, // STOP
+            0x11 => ops::ld_next_two_byte_into_reg_pair(self.mem, &mut self.reg.pc, &mut self.reg.d, &mut self.reg.e), // LD DE, nn
+            0x12 => ops::write_value_to_memory_at_address(self.mem, self.reg.a.read(), self.reg.d.read(), self.reg.e.read()), // LD (DE), A
+            0x13 => ops::increment_register_pair(&mut self.reg.d, &mut self.reg.e), // INC DE
+            0x14 => ops::increment_register(&mut self.reg.d, &mut self.reg.f), // INC D
+            0x15 => ops::decrement_register(&mut self.reg.d, &mut self.reg.f), // DEC D
             _ => return
         }
     }
