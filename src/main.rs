147141153b3e639dd51eaed4589fa0f008db9c55
Matt Murphy <matthew.john.murphy@gmail.com>
2014-11-03 21:11:14 -0600

    Remove glfw

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@
 #![feature(phase)]
 #[phase(plugin, link)] extern crate log;
 extern crate native;
-extern crate glfw;
 
-use glfw::Context;
 use memory::Memory;
 use cpu::Cpu;
 
@@ -17,38 +15,9 @@ mod cpu;
 fn main() {
     let mut memory = Memory::new(65536);
     let mut cpu = Cpu::new(&mut memory);
-    let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
-    let (window, events) = glfw.create_window(256, 256, "Hello this is window", glfw::Windowed)
-        .expect("Failed to create GLFW window.");
-
-    window.set_key_polling(true);
-    window.make_current();
-
-    while !window.should_close() {
-        glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            handle_window_event(&window, event);
-        }
-
-
+    loop {
         let clock_cycle = cpu.tick();
     }
-}
-
-
-//#[start]
-fn start(argc: int, argv: *const *const u8) -> int {
-    native::start(argc, argv, main)
-}
-
-fn handle_window_event(window: &glfw::Window, event: glfw::WindowEvent) {
-    match event {
-        glfw::KeyEvent(glfw::KeyEscape, _, glfw::Press, _) => {
-            window.set_should_close(true)
-        },
-        glfw::KeyEvent(glfw::KeyUp, _, glfw::Press, _) => {
-        },
-        _ => {}
-    }
+   
 }
